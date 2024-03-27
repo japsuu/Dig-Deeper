80c48b471026c63998e9e55fe8b740030d82bcf0
Jasper <japsu.honkasalo@gmail.com>
2024-03-27 13:51:41 +0200

    Add depth display gauge

--- src/Assets/Scripts/Constants.cs
+++ src/Assets/Scripts/Constants.cs
@@ -4,4 +4,6 @@
     public const int CHUNK_SIZE_PIXELS = 64;
     public const int CHUNK_SIZE_UNITS = CHUNK_SIZE_PIXELS / TEXTURE_PPU;
     public const int CHUNK_SIZE_UNITS_BITMASK = CHUNK_SIZE_UNITS - 1;
+    
+    public const int STATION_DEPTH_INTERVAL = 200;
 }
---
+++ src/Assets/Scripts/UI/DepthDisplay.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Entities.Drill;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class DepthDisplay : MonoBehaviour
+    {
+        [SerializeField]
+        private TMP_Text _text;
+
+        [SerializeField]
+        private Image _depthImage;
+
+        [SerializeField]
+        private Image _stationProgressImage;
+
+
+        private void Update()
+        {
+            const float stationInterval = Constants.STATION_DEPTH_INTERVAL;
+            
+            float playerDepth = -DrillController.Instance.transform.position.y;
+            float depthProgress = playerDepth % stationInterval;
+            float stationProgress = depthProgress / stationInterval;
+
+            float depthImageRotation = playerDepth < 0 ? 180 : 0;
+            _depthImage.rectTransform.localRotation = Quaternion.Euler(0, 0, depthImageRotation);
+
+            int depth = Mathf.RoundToInt(playerDepth);
+            _text.text = $"{depth:n0}m";
+            
+            _stationProgressImage.fillAmount = Mathf.Clamp01(stationProgress);
+        }
+    }
+}
