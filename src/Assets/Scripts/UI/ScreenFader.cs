8c1cf03b6f1d137644d142d25443e2d92e65a6bf
Jasper <japsu.honkasalo@gmail.com>
2024-04-02 14:11:52 +0300

    Prepare project for bootstrapping

---
+++ src/Assets/Scripts/UI/ScreenFader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using DG.Tweening;
+using Singletons;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ScreenFader : SingletonBehaviour<ScreenFader>
+    {
+        [SerializeField]
+        private Image _fadeImage;
+        
+        [SerializeField]
+        private float _fadeSpeed = 2f;
+        
+        
+        private IEnumerator Start()
+        {
+            _fadeImage.enabled = true;
+            _fadeImage.color = Color.black;
+            yield return new WaitForSeconds(1f);
+         
+            _fadeImage.DOFade(0f, _fadeSpeed).onComplete += () => _fadeImage.enabled = false;
+        }
+        
+        
+        public void EndScene(int sceneNumber)
+        {
+            _fadeImage.enabled = true;
+            _fadeImage.color = Color.clear;
+            _fadeImage.DOFade(1f, _fadeSpeed).onComplete += () => SceneManager.LoadScene(sceneNumber);
+        }
+    }
+}
