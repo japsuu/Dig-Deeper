151ef0dab51b2d5e73afe21b806ea43960eeb945
Jasper <japsu.honkasalo@gmail.com>
2024-04-04 10:04:13 +0300

    Add lights to multiple VFX

---
+++ src/Assets/Scripts/World/Light2DFade.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace World
+{
+    public class Light2DFade : MonoBehaviour
+    {
+        [SerializeField]
+        private Light2D _light;
+        
+        [SerializeField]
+        private float _fadeLength = 1f;
+        
+        
+        private void Start()
+        {
+            DOTween.To(() => _light.intensity, x => _light.intensity = x, 0f, _fadeLength).SetEase(Ease.Linear);
+        }
+    }
+}
