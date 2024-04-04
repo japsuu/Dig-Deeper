265b4b5fca7dc40d4df6901f0ce1791880033554
Jasper <japsu.honkasalo@gmail.com>
2024-04-04 13:00:02 +0300

    Add hover tooltips.

---
+++ src/Assets/Scripts/World/HealthPickup.cs
@@ -0,0 +1,24 @@
+﻿using Entities;
+using UnityEngine;
+
+namespace World
+{
+    public class HealthPickup : MonoBehaviour
+    {
+        [SerializeField] private int _healAmount = 500;
+        [SerializeField] private LayerMask _collisionMask;
+        
+        
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (_collisionMask != (_collisionMask | (1 << other.gameObject.layer)))
+                return;
+
+            if (!other.TryGetComponent(out EntityHealth health))
+                return;
+            
+            health.Heal(_healAmount);
+            Destroy(gameObject);
+        }
+    }
+}
